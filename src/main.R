9e1fc7b2c35701951e15bca361601cb3d578f941
Boris Chen <boris.chen@gmail.com>
2015-08-11 08:37:36 -0700

    preseason updates

--- src/main.R
+++ src/main.R
@@ -27,9 +27,14 @@ download.data(c('qb','rb','wr','te'))
 download.data(c('flex','k','dst'))
 download.data(c('ppr-rb','ppr-wr','ppr-te','ppr-flex'))
 download.data(c('half-point-ppr-rb','half-point-ppr-wr','half-point-ppr-te','half-point-ppr-flex'))
-download.data(c('ros-qb','ros-rb','ros-wr','ros-te'))
-download.data(c('ros-flex','ros-k','ros-dst'))
-download.data(c('ros-ppr-rb','ros-ppr-wr','ros-ppr-te','ros-ppr-flex'))
+
+if (FALSE) {
+	#download.data(c('ros-qb','ros-rb','ros-wr','ros-te'))
+	#download.data(c('ros-flex','ros-k','ros-dst'))
+	#download.data(c('ros-ppr-rb','ros-ppr-wr','ros-ppr-te','ros-ppr-flex'))
+}
+
+download.predraft.data()
 
 ## If there are any injured players, list them here to remove them
 injured <- c('')
@@ -47,32 +52,22 @@ draw.tiers("dst", 1, 26, 6, XLOW=5)
 draw.tiers("ppr-rb", 1, 40, 11)
 draw.tiers("ppr-wr", 1, 60, 9, highcolor=500)
 draw.tiers("ppr-te", 1, 30, 8)
-draw.tiers("ppr-flex", 1, 80, 14, XLOW=5, highcolor=650)
+#draw.tiers("ppr-flex", 1, 80, 14, XLOW=5, highcolor=650)
 
 draw.tiers("half-point-ppr-rb", 1, 40, 9)
 draw.tiers("half-point-ppr-wr", 1, 60, 10, highcolor=400)
 draw.tiers("half-point-ppr-te", 1, 30, 7)
-draw.tiers("half-point-ppr-flex", 1, 80, 15, XLOW=5, highcolor=650)
+#draw.tiers("half-point-ppr-flex", 1, 80, 15, XLOW=5, highcolor=650)
 
 injured <- c('')
 
-draw.tiers("ros-qb", 1, 32, 7, highcolor=360)
-draw.tiers("ros-rb", 1, 50, 12, highcolor=500)
-draw.tiers("ros-wr", 1, 64, 65/5, highcolor=550, XLOW=5)
-draw.tiers("ros-te", 1, 30, 7, XLOW=5)
-draw.tiers("ros-k", 1, 20, 5, XLOW=5)
-draw.tiers("ros-dst", 1, 25, 5, XLOW=5)
-
-
-
-draw.tiers("ros-ppr-rb", 1, 40, 10, highcolor=450)
-draw.tiers("ros-ppr-wr", 1, 60, 10, highcolor=400, XLOW=5)
-draw.tiers("ros-ppr-te", 1, 21, 6, XLOW=5)
 
 # PRESEASON
-COMMENT <- function() {
+
 source('~/projects/fftiers/src/ff-functions.R')
+download.predraft.data()
 injured <- c('')
+
 draw.tiers("all", 1, 68, 10, XLOW=5, highcolor=720)
 draw.tiers("all", 69, 141, 7, adjust=10, XLOW=18, highcolor=720)
 draw.tiers("all", 142, 210, 4, adjust=17, XLOW=20, highcolor=500)
@@ -84,4 +79,17 @@ draw.tiers("all-ppr", 141, 200, 5, adjust=16, XLOW=30)
 draw.tiers("all-half-ppr", 1, 75, 10, XLOW=5)
 draw.tiers("all-half-ppr", 76, 143, 6, adjust=10, XLOW=20)
 draw.tiers("all-half-ppr", 144, 200, 4, adjust=16, XLOW=30)
+
+
+comment <- function() {
+
+draw.tiers("ros-qb", 1, 32, 7, highcolor=360)
+draw.tiers("ros-rb", 1, 50, 12, highcolor=500)
+draw.tiers("ros-wr", 1, 64, 65/5, highcolor=550, XLOW=5)
+draw.tiers("ros-te", 1, 30, 7, XLOW=5)
+draw.tiers("ros-k", 1, 20, 5, XLOW=5)
+draw.tiers("ros-dst", 1, 25, 5, XLOW=5)
+draw.tiers("ros-ppr-rb", 1, 40, 10, highcolor=450)
+draw.tiers("ros-ppr-wr", 1, 60, 10, highcolor=400, XLOW=5)
+draw.tiers("ros-ppr-te", 1, 21, 6, XLOW=5)
 }
