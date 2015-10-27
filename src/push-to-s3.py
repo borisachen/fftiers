ea0426dd99b5ea34566c052d15200e2e36cb7f02
Boris Chen <boris.chen@gmail.com>
2015-10-27 12:17:25 -0700

    push code

---
+++ src/push-to-s3.py
@@ -0,0 +1,31 @@
+
+
+foo = 'aws s3 cp ~/projects/fftiers/out/current/png/weekly-QB.png s3://fftiers/out/weekly-QB.png'
+aws1 = 'aws s3 cp '
+aws2 = ' s3://fftiers/out/current/'
+
+import os
+
+def absoluteFilePaths(directory):
+	ans=[]
+	for dirpath,_,filenames in os.walk(directory):
+		for f in filenames:
+			ans.append(os.path.abspath(os.path.join(dirpath, f)))
+	return ans
+
+
+def FilesPaths(directory):
+	ans=[]
+	for dirpath,_,filenames in os.walk(directory):
+		for f in filenames:
+			ans.append(f)
+	return ans
+
+mypath = '/home/ubuntu/projects/fftiers/out/current/'
+files_to_push = absoluteFilePaths(mypath)
+file_list = FilesPaths(mypath)
+
+for i in range(0,len(files_to_push)):
+	exeline = aws1 + files_to_push[i] + aws2 + file_list[i]
+	os.system(exeline)
+
