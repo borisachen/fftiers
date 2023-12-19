73ed6e71d2ea39ff97ed824dec98548359be0804
borisachen <boris.chen@gmail.com>
2023-12-19 11:12:06 -0800

    add push cmd for python3

--- src/master.py
+++ src/master.py
@@ -15,6 +15,9 @@ head_path = set_path()
 cmd1 = 'Rscript %s/src/main.R t' % head_path
 os.system(cmd1)
 
-cmd2 = 'python %s/src/push-to-s3.py' % head_path
-os.system(cmd2)
+# cmd2 = 'python %s/src/push-to-s3.py' % head_path
+# os.system(cmd2)
+
+cmd3 = 'python3 %s/src/push-to-s3.py' % head_path
+os.system(cmd3)
 
